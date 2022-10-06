f4f207ee205acc3a4b79b09328f0c2fa798178c2
Александр Беликов <abs301216@gmail.com>
2022-10-06 14:47:33 +0300

    Корректировка DateTime.pas

---
+++ DateTime.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var (day1, day2) := readinteger2('Введите номера 2 дней (макс 366):');
+  if abs(365 - day1) < abs(365 - day2) then println(day1, 'ближе к новому году, чем', day2)
+  else println(day2, 'ближе к новому году, чем', day1)
+end.
